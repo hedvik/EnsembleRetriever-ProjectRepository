bc7ba229f78a0f73806533d268c3762c1c9c9d91
andrwan@stud.ntnu.no <andrwan@stud.ntnu.no>
2019-01-31 10:17:51 +0100

    Added Ensemble Retriever Assets
    Also added some initial work for reset world fading

---
+++ Assets/Scripts/RedirectionManagerER.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedirectionManagerER : RedirectionManager
+{
+    [Header("Ensemble Retriever Related")]
+    public MeshRenderer _environmentFadeCube;
+}
