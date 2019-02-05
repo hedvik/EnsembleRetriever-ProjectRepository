19831f8fb03d3bd09d2ad57d31d5b6b582187265
andrwan@stud.ntnu.no <andrwan@stud.ntnu.no>
2019-02-05 13:14:05 +0100

    Debug distractor now pauses with coroutines

--- Assets/Scripts/DistractorScripts/DebugDistractor.cs
+++ Assets/Scripts/DistractorScripts/DebugDistractor.cs
@@ -9,27 +9,30 @@ public class DebugDistractor : Distractor
     public float _lifeTime = 5f;
     public float _rotationSpeed = 5f;
 
-    private float _lifeTimer = 0f;
-
     public override void InitialiseDistractor(RedirectionManagerER redirectionManager)
     {
         base.InitialiseDistractor(redirectionManager);
 
         transform.position = _redirectionManagerER.headTransform.position + _redirectionManagerER.headTransform.forward * _forwardOffsetFromPlayer;
         _spawnParticles.Play();
+
+        StartCoroutine(RotateAroundPlayer());
     }
 
-    void Update()
+    private IEnumerator RotateAroundPlayer()
     {
-        if (!_isPaused)
-        {
-            _lifeTimer += Time.deltaTime;
-            transform.RotateAround(_redirectionManagerER.headTransform.position, Vector3.up, _rotationSpeed * Time.deltaTime);
+        var timer = 0f;
 
-            if (_lifeTimer >= _lifeTime)
+        while(timer < _lifeTime)
+        {
+            if (!_isPaused)
             {
-                _redirectionManagerER.OnDistractorEnd();
+                timer += Time.deltaTime;
+                transform.RotateAround(_redirectionManagerER.headTransform.position, Vector3.up, _rotationSpeed * Time.deltaTime);
             }
+            yield return null;
         }
+
+        _redirectionManagerER.OnDistractorEnd();
     }
 }
