89b0d97eebd2bd39ed28682502e3f2a162d35272
unknown <jesushl@stud.ntnu.no>
2019-02-06 10:35:00 +0100

    Integrated current solution with VR

---
+++ Assets/Scripts/Utility/DisableVR.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class DisableVR : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        XRSettings.enabled = false;
+    }
+}
