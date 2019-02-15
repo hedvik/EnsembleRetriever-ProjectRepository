e2e492c8bc87fe9b9a0ea073338fcf7c63369057
Andreas Wang <and-wan@hotmail.com>
2019-02-15 14:42:58 +0100

    Added idle and idle break animations to hint givers

--- Assets/Scripts/HintGiver.cs
+++ Assets/Scripts/HintGiver.cs
@@ -2,16 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HintGiver : MonoBehaviour
+public class HintGiver : Pausable
 {
+    public float _randomAnimationTriggerCooldown = 4f;
+    public float _randomAnimationTriggerNoise = 2f;
+
     private GameManager _gameManager;
     private GameObject _textBoxObject;
+    private Animator _animator;
+    private float _triggerTimer = 0f;
+    private float _randomTimeTarget;
 
     private void Start()
     {
         _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         _textBoxObject = transform.Find("TextBox").gameObject;
         _textBoxObject.transform.localScale = Vector3.zero;
+        _animator = GetComponentInChildren<Animator>();
+        _animator.Play("Idle", -1, Random.Range(0f, 1f));
+
+        _randomTimeTarget = Random.Range(_randomAnimationTriggerCooldown - _randomAnimationTriggerNoise, _randomAnimationTriggerCooldown + _randomAnimationTriggerNoise);
+    }
+
+    private void Update()
+    {
+        _triggerTimer += Time.deltaTime;
+
+        if(_triggerTimer >= _randomTimeTarget)
+        {
+            _triggerTimer = 0;
+            _randomTimeTarget = Random.Range(_randomAnimationTriggerCooldown - _randomAnimationTriggerNoise, _randomAnimationTriggerCooldown + _randomAnimationTriggerNoise);
+            _animator.SetTrigger("Break" + Random.Range(1, 4).ToString());
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -28,5 +50,10 @@ public class HintGiver : MonoBehaviour
         {
             _gameManager._uiManager.ChangeTextBoxVisibility(false, _textBoxObject.transform);
         }
+    }
+
+    protected override void PauseStateChange()
+    {
+        _animator.enabled = !_isPaused;
     }
 }
